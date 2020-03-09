2d1bcb37c0efcbb18bb8c1e047d312e672b6df58
Wolfjawan <mohsen000069@gmail.com>
2020-03-09 14:22:02 +0000

    new

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './src/index';
 import { name as appName } from './app.json';
+import WidgetTask from "./widgetTask";
 
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerHeadlessTask('WidgetTask', () => WidgetTask);
